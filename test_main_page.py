843dd75eb849b12d150a9b8bbf40c8e00903a899
Igor Merkushev <igor.merkushev@auto1.com>
2020-05-08 21:04:29 +0200

    add loginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,12 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
