c528233e69b2a4d1487e7b50ac12988134d4f4f6
Igor Merkushev <igor.merkushev@auto1.com>
2020-05-10 19:32:00 +0200

    rm unneccesary links

--- test_product_page.py
+++ test_product_page.py
@@ -13,16 +13,8 @@ class TestAddToBasketFromProductPage:
 
     @pytest.mark.need_review
     @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
                                       pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-                                                   "/?promo=offer7", marks=pytest.mark.xfail),
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+                                                   "/?promo=offer7", marks=pytest.mark.xfail),])
     def test_guest_can_add_product_to_basket(self, browser: WebDriver, link: str):
         page = ProductPage(browser, link)
         page.open()
